456a36a4585b1c6259287aeb2a5359868e370013
RohSungKyun <songkyun1108@naver.com>
2024-07-15 11:46:35 +0900

    [feat] 공지사항 조건부렌더링

--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -1,13 +1,23 @@
 import "./Viewer.css";
 
 
-const Viewer = ({username, created_at, place, description}) => {
+const Viewer = ({username, created_at, place, description, mode}) => {
     return (
         <div className="viewer">
             <div className="viewInfo">
-                <p>작성자 : {username}</p>
-                <p>Date : {new Date(created_at).toLocaleDateString()}</p>
-                <p>장소 : {place}</p>
+            {mode !== "notice" && (
+                    <>
+                        <p>작성자 : {username}</p>
+                        <p>Date : {new Date(created_at).toLocaleDateString()}</p>
+                        <p>장소 : {place}</p>
+                    </>
+                )}
+                {/* mode가 notice이면 날짜 정보만 보임 */}
+                {mode === "notice" && (
+                    <>
+                        <p>Date : {new Date(created_at).toLocaleDateString()}</p>
+                    </>
+                )}
             </div>
             <div className="descSection">
                 <pre>{description}</pre>
