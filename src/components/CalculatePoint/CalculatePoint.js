9a456dc9f854c34efc27729bc68b601c1ed76129
pkyujin <luxoris02@sookmyung.ac.kr>
2024-07-16 13:01:17 +0900

    [chore] 모달 스타일 수정

--- src/components/CalculatePoint/CalculatePoint.js
+++ src/components/CalculatePoint/CalculatePoint.js
@@ -3,12 +3,15 @@ import "./CalculatePoint.css";
 
 const CalculatePoint = ({ show, onClose, weekScore }) => {
   return (
-    <div className={`modal ${show ? "show" : ""}`}>
-      <div className="cal-message-design">확인 메시지</div>
-      <div className="cal-modal-content">
-        <p>{weekScore} 포인트가 적립되었습니다.</p>
-        <button className="cal-ok-button" onClick={onClose}>확인</button>
+    <div className={`cal-modal ${show ? "show" : ""}`}>
+      <div className="calWrapper">
+       <p className="calTitle">확인 메시지</p>
+       <div className="calContent">
+         <p>{weekScore} 포인트가 적립되었습니다.</p>
+         <button className="cal-ok-button" onClick={onClose}>확인</button>
+       </div>
       </div>
+      
     </div>
   );
 };
--- src/components/FinishStudyModal/FinishStudyModal.js
+++ src/components/FinishStudyModal/FinishStudyModal.js
@@ -5,12 +5,15 @@ import "../DeleteID/DeleteID.css";
 const FinishStudyModal = ({ onClose }) => {
   return (
     <div className="finish-modal">
-      <div className="finish-message-design">확인 메시지</div>
-        <div className="finish-modal-content">
+      <div className="finishWrapper">
+      <div className="finishTitle">확인 메시지</div>
+        <div className="finishContent">
           <p>스터디가 종료되었습니다. <br />수고하셨습니다!</p>
           <button className="finish-close-button" onClick={onClose}>닫기</button>
         </div>
-      </div>   
+      </div> 
+      </div>
+        
   );
 };
 
